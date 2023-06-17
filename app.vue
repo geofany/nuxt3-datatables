b668a2791e3a034a68918ee62a4e6d87e1cc0575
geofany <geofanygalindra@gmail.com>
2023-06-17 14:17:28 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <DataTable :data="[[1, 2], [3, 4]]" class="display">
+    <thead>
+      <tr>
+        <th>A</th>
+        <th>B</th>
+      </tr>
+    </thead>
+  </DataTable>
+</template>
+
+<script setup lang="ts">
+import DataTable from 'datatables.net-vue3'
+import DataTablesLib from 'datatables.net';
+
+DataTable.use(DataTablesLib);
+</script>
+
+<style scoped></style>
